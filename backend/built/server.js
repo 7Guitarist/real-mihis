44c83263d9a476a15cb79732f8fcc80da06ae127
johndoe <johndoe@gmail.com>
2024-08-19 23:29:38 +0800

    problem fix

---
+++ backend/built/server.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var path_1 = __importDefault(require("path"));
+var cors_1 = __importDefault(require("cors"));
+var app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    credentials: true,
+    origin: ["http://localhost:4200"],
+}));
+app.get("*", function (req, res) {
+    res.sendFile(path_1.default.join(__dirname, "public", "index.html"));
+});
+var port = 5000;
+app.listen(port, function () {
+    console.log("Website served on http://localhost:" + port);
+});
